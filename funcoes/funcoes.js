1ddef356167415f891751c7623b11a2c18b3a73e
MateusFil <mateuswk@outlook.com>
2023-06-29 21:39:36 -0300

    Praticando funções

---
+++ funcoes/funcoes.js
@@ -0,0 +1,12 @@
+function DigitesuaIdade (idade) {
+if (idade >=18){ 
+console.log ('Você é maior de idade ' + idade);
+ }
+else if (idade < 18){ 
+console.log('Você é menor de idade ' + idade);
+}
+
+return
+
+}
+DigitesuaIdade (15);
